5443fe5c55e9a3d6208684d71684b4beaa11a29c
nannaphasb <nannaphas.bc@gmail.com>
2024-05-30 17:26:09 +0700

    Add solutions in Section 1

---
+++ section1/08_Two timestamps.py
@@ -0,0 +1,8 @@
+f_h = int(input())
+f_m = int(input())
+f_s = int(input())
+s_h = int(input())
+s_m = int(input())
+s_s = int(input())
+
+print((s_s + s_m*60 + s_h*60*60) - (f_s + f_m*60 + f_h*60*60))
